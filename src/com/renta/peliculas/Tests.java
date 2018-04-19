6d11692497bf75da3a9ce2898d0b0ed5f10a6346
D-Rod <danrodret@outlook.com>
2018-04-19 09:18:56 -0600

    Prueba exitosa

--- src/com/renta/peliculas/Tests.java
+++ src/com/renta/peliculas/Tests.java
@@ -5,9 +5,16 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class Tests {
+
 	@Test
-	public boolean pruebaUno() {
-	return 0;
+	public void pruebaUno() {
+		Pelicula movie = new Pelicula("Pirates of the Caribbean", 0);
+		Disco disc = new Disco("1234", movie);
+		Alquiler rental = new Alquiler(disc, 22);
+		Cliente cliente = new Cliente("Daniel");
+
+		cliente.addRental(rental);
+		cliente.statement();
 	}
 
 }
