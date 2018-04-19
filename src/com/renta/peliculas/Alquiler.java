f183b738fcf5530f5441ad3ed80bad2bb54d1ebd
D-Rod <danrodret@outlook.com>
2018-04-19 09:33:02 -0600

    Fix de datos que se reciben

--- src/com/renta/peliculas/Alquiler.java
+++ src/com/renta/peliculas/Alquiler.java
@@ -1,20 +1,20 @@
 package com.renta.peliculas;
 
 public class Alquiler {
-    private Disco disco;
-    private int diasAlquilado;
-	
+	private Disco disco;
+	private int diasAlquilado;
+
 	public int getDiasAlquilado() {
-    	return diasAlquilado;
-    }
-    public Disco getDisco() {
-    	return disco;
-    }
-    
-    public Alquiler(Disco pDisco, int pDiasAlquilado) {
-    	disco = pDisco;
-    	diasAlquilado = pDiasAlquilado;
-    }
-    
+		return diasAlquilado;
+	}
+
+	public Disco getDisco() {
+		return disco;
+	}
+
+	public Alquiler(Disco pDisco, int pDiasAlquilado) {
+		disco = pDisco;
+		diasAlquilado = pDiasAlquilado;
+	}
 
 }
