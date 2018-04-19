906c7854613e4931558e08842fbf12eee46cfe7e
D-Rod <danrodret@outlook.com>
2018-04-19 09:42:16 -0600

    Fix: Constructor y dato

--- src/com/renta/peliculas/Cliente.java
+++ src/com/renta/peliculas/Cliente.java
@@ -4,67 +4,74 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Cliente {
+	public Cliente(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
 	private String nombre;
-	
-	public Cliente(String pNombre) {
-        nombre = pNombre;
-    }
-	
-    public String statement() {
-        double montoTotal = 0;
-        int puntosAlquilerFrecuente = 0;
-        Iterator<Alquiler> iterator= alquileres.iterator();
-        String result = "Alquileres de " + nombre + ":\n";
-        while (iterator.hasNext()) {
-            double montoAlquiler = 0;
-            Alquiler alquiler = iterator.next();
 
-            //determine amounts for each line
-            switch (alquiler.getDisco().getPelicula().getCodigoPrecio()) {
-                case Pelicula.NORMAL:
-                    montoAlquiler += 2;
-                    if (alquiler.getDiasAlquilado() > 2)
-                        montoAlquiler += (alquiler.getDiasAlquilado() - 2) * 1.5;
-                    break;
-                case Pelicula.ESTRENO:
-                    montoAlquiler += alquiler.getDiasAlquilado() * 3;
-                    break;
-                case Pelicula.INFANTIL:
-                    montoAlquiler += 1.5;
-                    if (alquiler.getDiasAlquilado() > 3)
-                        montoAlquiler += (alquiler.getDiasAlquilado() - 3) * 1.5;
-                    break;
+	public String statement() {
+		double montoTotal = 0;
+		int puntosAlquilerFrecuente = 0;
+		Iterator<Alquiler> iterator = alquileres.iterator();
+		String result = "Alquileres de " + nombre + ":\n";
+		while (iterator.hasNext()) {
+			double montoAlquiler = 0;
+			Alquiler alquiler = iterator.next();
 
-            }
-            montoTotal += montoAlquiler;
+			// determine amounts for each line
+			switch (alquiler.getDisco().getPelicula().getCodigoPrecio()) {
+			case Pelicula.NORMAL:
+				montoAlquiler += 2;
+				if (alquiler.getDiasAlquilado() > 2)
+					montoAlquiler += (alquiler.getDiasAlquilado() - 2) * 1.5;
+				break;
+			case Pelicula.ESTRENO:
+				montoAlquiler += alquiler.getDiasAlquilado() * 3;
+				break;
+			case Pelicula.INFANTIL:
+				montoAlquiler += 1.5;
+				if (alquiler.getDiasAlquilado() > 3)
+					montoAlquiler += (alquiler.getDiasAlquilado() - 3) * 1.5;
+				break;
 
-            puntosAlquilerFrecuente ++;
-            // agregar bono por alquiler de pelicula "estreno"
-            if ((alquiler.getDisco().getPelicula().getCodigoPrecio() == Pelicula.ESTRENO) && alquiler.getDiasAlquilado() > 1) puntosAlquilerFrecuente ++;
+			}
+			montoTotal += montoAlquiler;
 
-            //mostrar datos
-            result += "\t" + alquiler.getDisco().getPelicula().getNombre()+ "\t" + String.valueOf(montoAlquiler) + "\n";
+			puntosAlquilerFrecuente++;
+			// agregar bono por alquiler de pelicula "estreno"
+			if ((alquiler.getDisco().getPelicula().getCodigoPrecio() == Pelicula.ESTRENO)
+					&& alquiler.getDiasAlquilado() > 1)
+				puntosAlquilerFrecuente++;
 
-        }
-        //fin del reporte
-        result +=  "Monto total:  " + String.valueOf(montoTotal) + "\n";
-        result += "Gano " + String.valueOf(puntosAlquilerFrecuente) + " puntos por alquiler frecuente";
-        return result;
+			// mostrar datos
+			result += "\t" + alquiler.getDisco().getPelicula().getNombre() + "\t" + String.valueOf(montoAlquiler)
+					+ "\n";
 
-    }
+		}
+		// fin del reporte
+		result += "Monto total:  " + String.valueOf(montoTotal) + "\n";
+		result += "Gano " + String.valueOf(puntosAlquilerFrecuente) + " puntos por alquiler frecuente";
+		return result;
+
+	}
+
 	public String getNombre() {
 		return nombre;
 	}
-    
-    
-    public void addRental(Alquiler arg) {
-    	alquileres.add(arg);
-    }
-    public static Cliente get(String name) {
-    	return (Cliente) Registrar.get("Clientes", name);
-    }
-    public void persist() {
-    	Registrar.add("Clientes", this);
-    }
-    private ArrayList<Alquiler> alquileres= new ArrayList<>();
+
+	public void addRental(Alquiler arg) {
+		alquileres.add(arg);
+	}
+
+	public static Cliente get(String name) {
+		return (Cliente) Registrar.get("Clientes", name);
+	}
+
+	public void persist() {
+		Registrar.add("Clientes", this);
+	}
+
+	private ArrayList<Alquiler> alquileres = new ArrayList<>();
 }
